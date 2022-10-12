f087f91e70fb5a3e39a12cf719db175a23aeeb59
paul serafim <paul.serafim.3021@gmail.com>
2022-10-12 13:52:05 +1100

    code

--- questions/binarySearch/index.js
+++ questions/binarySearch/index.js
@@ -13,7 +13,22 @@
  */
 
 const binarySearch = (nums, target) => {
+    let l = 0;
+    let r = nums.length - 1;
 
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+
+        if (nums[mid] > target) {
+            r = mid - 1;
+        }
+        else if (nums[mid] < target) {
+            l = mid + 1
+        }
+        else {
+            return mid
+        }
+    }
 
 }
 
