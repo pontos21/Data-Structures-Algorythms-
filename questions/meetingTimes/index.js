8f73c4df996dee60447e093f946117ac3f9c40da
paul serafim <paul.serafim.3021@gmail.com>
2022-10-12 12:25:09 +1100

    code

--- questions/meetingTimes/index.js
+++ questions/meetingTimes/index.js
@@ -11,7 +11,17 @@
  */
 
  const meetingTimes = (times) => {
+    times.sort((a, b) => a[0] - b[0])
 
+    for (let i = 1; i <times.length; ++i) {
+        const currentStart = times[i][0]
+        const prevEnd = times[i - 1][1]
+
+        if (currentStart < prevEnd) {
+            return false
+        }
+    }
+    return true 
  };
 
 module.exports = meetingTimes;
